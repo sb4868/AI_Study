ed3fe5a13d57c153620321718f22ad2ca3691a5f
sb4868 <79896430+sb4868@users.noreply.github.com>
2021-08-05 00:24:23 +0900

    Create Logistic_regression.py

---
+++ Machine_Learning/Logistic_regression.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+x_data = np.array([40, 50, 60, 70, 80, 90]).reshape(6,1)
+t_data = np.array([0, 0, 0, 1, 1, 1]).reshape(6,1)
+
+W = np.random.rand(1,1)
+b = np.random.rand(1)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def cost(x, t):
+    delta = 1e-7 
+    z = np.dot(x, W) + b
+    y = sigmoid(z)
+    return -np.sum(t*np.log(y + delta) + (1-t)*np.log((1-y) + delta))
+
+def numerical_derivative(f, x):
+    delta_x = 1e-4
+    grad = np.zeros_like(x)     
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+    
+        x[idx] = float(tmp_val) + delta_x
+        fx1 = f(x)
+        x[idx] = tmp_val - delta_x
+        fx2 = f(x)
+        
+        grad[idx] = (fx1 - fx2) / (2 * delta_x)
+        x[idx] = tmp_val
+        it.iternext()
+        
+    return grad
+
+    
+f = lambda x : cost(x_data, t_data)
+
+learning_rate = 1e-3
+
+for step in range(80001):
+    W -= learning_rate * numerical_derivative(f, W)
+    b -= learning_rate * numerical_derivative(f, b)
+    if (step%1000 == 0):
+        print("step =", step, ", error value :", cost(x_data, t_data), ", W =",W, ", b =",b)
+        
+
+def test(x):
+    z = np.dot(x, W) + b
+    y = sigmoid(z)
+    if y > 0.5:
+        result = 1
+    else:
+        result = 0
+    return y, result
